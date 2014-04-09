4eeaf51a9a603c45663a27e182c0311512f6b54d
DoktorAerzt <bohnet.michael@web.de>
2014-04-09 15:20:36 +0200

    1. Kapitel Abgeschlossen
    Das erste Kapitel ist hiermit abgeschlossen. Hoffentlich ist nichts zu
    verbessern.

---
+++ Kaptel 1/zweite_aufgabe_schleifchen.java
@@ -0,0 +1,13 @@
+
+public class zweite_aufgabe_schleifchen {
+	public static void main (String[] args) {
+		int x = 1;
+		System.out.println("Vor der Schleife");
+		while (x < 4) {
+			System.out.println("In der Schleife");
+			System.out.println("Der Wert von x ist " + x);
+			x = x +1;
+		}
+		System.out.println("Nach der Schleife");
+	}
+}
